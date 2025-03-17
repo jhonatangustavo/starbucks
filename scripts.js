bb754cf1c1c1ea40a2d814a448b61d93e8ffa908
jhonatangustavo <jhonatangustavo500@gmail.com>
2025-03-17 17:18:53 -0300

    Starbucks

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector(".imagem-copo")
+
+function trocaimagem(endereco) {
+    imagem.src = endereco
+
+}
+
+let circulo = document.querySelector(".circulo")
+
+function trocacor(cor) {
+    circulo.style.background = cor
+}
